9e9ffef05ab045f841324dce1783f3cf7c86bba1
vadossus <157449023+vadossus@users.noreply.github.com>
2024-12-05 22:13:47 +0500

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    char day[25];
+    char time_begin[25];
+    char time_end[25];
+
+    std::cout << "День: ";
+    std::cin >> day;
+    std::cout << "Время начала: ";
+    std::cin >> time_begin;
+    std::cout << "Время окончания: ";
+    std::cin >> time_end;
+
+    FILE* file;
+    errno_t err = fopen_s(&file, "1.txt", "a");
+
+    if (err != 0 || file == NULL) {
+        std::cout << "Файл невозможно открыть или создать" << std::endl;
+    }
+    else {
+        fprintf(file, "%s | %s | %s\n", day, time_begin, time_end);
+        fclose(file);
+        std::cout << "Файл записан" << std::endl;
+    }
+
+    _getch();
+    return 0;
+}
